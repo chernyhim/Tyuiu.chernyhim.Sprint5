b811c99865ae24c872d0aa08455afd1a16ba95bc
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-11-20 20:36:06 +0500

    Task4

--- Tyuiu.chernyhim.Sprint5.Task4.V17.Lib/DataService.cs
+++ Tyuiu.chernyhim.Sprint5.Task4.V17.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.chernyhim.Sprint5.Task4.V17.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
+            strx = strx.Replace(".",",");
             double strxd = Convert.ToDouble(strx);
             double result = Math.Round((Math.Sin(2/(3*strxd))+strxd*strxd),3);
             return result;
